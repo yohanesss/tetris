40241f29083265521dcfab0048a742e477a0a415
Yohanes Setiawan <test27c@gmail.com>
2021-05-14 18:06:00 +0700

    change logic bgm

--- src/components/Bgm.tsx
+++ src/components/Bgm.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import tetrisMp3 from "../assets/tetris.mp3";
 import Sound from "react-sound";
 import * as S from "../styles";
 
 export const Bgm = () => {
-    const [isPlaying, setIsPlaying] = useState(true);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        setIsPlaying(true);
+    }, []);
+
     return (
         <>
             <Sound
